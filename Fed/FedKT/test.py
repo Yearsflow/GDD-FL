52e6fa61d06da7739439e661e556158fc9c5d80a
Yearsflow <yearsflow@126.com>
2023-11-05 11:02:39 +0000

    update FedKT codes

---
+++ Fed/FedKT/test.py
@@ -0,0 +1,6 @@
+from dataset_utils import MNIST_truncated
+from torch.utils.data import DataLoader
+
+a = MNIST_truncated(root='./data')
+b = DataLoader(a, batch_size=100, shuffle=True)
+print(b.dataset)
