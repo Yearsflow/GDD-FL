a58c98886c48a00171c0329499d34ee5e9867bdd
Yearsflow <yearsflow@126.com>
2023-11-08 09:26:16 +0000

    add fedgan and remove BigGAN

--- BigGAN/scripts/prepare_data.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-python make_hdf5.py --dataset isic2020 --batch_size 64 --data_root /data/tyc/dataset
-python calculate_inception_moments.py --dataset isic2020 --data_root /data/tyc/dataset
