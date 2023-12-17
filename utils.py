81654d624d1c70a3a22729eb71907eb547fa3956
Yearsflow <yearsflow@126.com>
2023-12-17 15:16:58 +0000

    fix bugs from utils

--- utils.py
+++ utils.py
@@ -18,7 +18,7 @@ def seed_everything(seed):
         torch.cuda.manual_seed(seed)
         torch.cuda.manual_seed_all(seed)
     random.seed(seed)
-    torch.backends.cudnn.benchmark=True
+    torch.backends.cudnn.benchmark = False
     torch.backends.cudnn.deterministic = True
 
 def mkdirs(dirpath):
