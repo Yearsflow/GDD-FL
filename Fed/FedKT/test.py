a070086e27cb00752f60b58a0155e9171b52ca5f
Yearsflow <yearsflow@126.com>
2024-03-11 02:02:11 +0000

    remove some files

--- Fed/FedKT/test.py
+++
@@ -1,6 +0,0 @@
-from utils.dataset_utils import MNIST_truncated
-from torch.utils.data import DataLoader
-
-a = MNIST_truncated(root='./data')
-b = DataLoader(a, batch_size=100, shuffle=True)
-print(b.dataset)
