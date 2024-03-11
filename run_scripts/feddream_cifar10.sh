13df3aee1d1cf07f220cd90b2e530a61007459d3
Yearsflow <yearsflow@126.com>
2024-03-11 02:01:13 +0000

    remove some files

--- run_scripts/feddream_cifar10.sh
+++
@@ -1,9 +0,0 @@
-python main.py --mode Fed \\
-               --approach feddream \\
-               --dataset cifar10 \\
-               --device 0 \\
-               --inner_loop 10 \\
-               --metric mse \\
-               --lr_img 5e-3 \\
-               --mix_p 0.5 \\
-               --factor 2
